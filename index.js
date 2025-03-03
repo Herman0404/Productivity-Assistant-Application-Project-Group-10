f678359d768e6256068507ee4fab894c35de5b8b
Herman0404 <herminator04@gmail.com>
2025-03-03 09:31:31 +0100

    readyfortesting

--- index.js
+++ index.js
@@ -1,7 +1,74 @@
+document.addEventListener('DOMContentLoaded', function() {
+    displayTasks();
+    displayEvents();
+})
+
+function displayTasks(){
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || []; //parse the string to convert it to an array
+    let taskList = document.querySelector("#tasks"); //this is the ul element where the tasks will be displayed
+    
+    // clear the existing list
+    taskList.innerHTML = "";
+
+    // creates array with only not-completed tasks
+    filteredTasks = tasks.filter(task => {
+        let notCompleted = task.status === "not-completed";
+        console.log(notCompleted);
+        return notCompleted;
+    });
+
+    showTasks = getClosestDates(filteredTasks);
+
+    if(showTasks.length != 0){
+        //loop through the tasks array and display each task on the screen
+        showTasks.forEach(task => { 
+            //tasks.forEach((task, index) => {
+            let li = document.createElement("li");
+            li.classList.add("list-item");
+        
+            // Task status display (Updates dynamically)
+            li.innerHTML = `
+                <div class="task-details">
+                    <h3><strong>${task.title}</strong></h3>
+                    <p><strong>Description:</strong> ${task.description}</p>
+                    <p><strong>Time Estimate:</strong> ${task.timeEstimate} hours</p>
+                    <p><strong>Category:</strong> ${task.category}</p>
+                    <p><strong>Deadline:</strong> ${task.deadline}</p>
+                </div>`;
+            taskList.appendChild(li);
+        });    
+    } else {
+        let li = document.createElement("li");
+        li.classList.add("list-item")
+
+        li.innerHTML = `
+        <div class="task-details">
+            <h3><strong>No uncompleted tasks</strong></h3>
+        `
+        taskList.appendChild(li);
+    }
+    
+}
+
+
+function getClosestDates(tasks) {
+    const now = Date.now();
+    return tasks
+        // Sorts based on when they were added 
+        .sort((a, b) => Math.abs(Number(a.date) - now) - Math.abs(Number(b.date) - now))
+        // Saves closest 3 
+        .slice(0, 3); 
+}
+
+
+// Event display
 function displayEvents(){
+    // Array with value
     let events = (JSON.parse(localStorage.getItem("events")) || []);
-    let notPassedEvents = []
-    let currentDate = Date.now()
+    let notPassedEvents = [];
+    // Takes current date to compare
+    let currentDate = Date.now();
+    // Ads every event that hasnt started into an array
     events.forEach(event => {
         event.startDate = new Date(event.startDate);
         event.endDate = new Date(event.endDate);
@@ -10,38 +77,49 @@ function displayEvents(){
         }
     });
 
+    // Takes only the 3 upcoming events
     let showEvents = notPassedEvents.slice(0, 3);
     let eventList = document.querySelector("#events");
     eventList.innerHTML = "";
 
-    showEvents.forEach(event => {
-        
-        // variable to create a new list inside event
-        let li = document.createElement("li");
-        // adds class "event-item" to the item
-        li.classList.add("event-item");
-        
-        // makes sure the format is displayed as yyyy-mm-dd hh:mm
-        const timeFormat = ("sv-SE", {
-            hour: "2-digit",
-            minute: "2-digit",
+    // Displays events
+    if(showEvents != 0){
+        showEvents.forEach(event => { 
+            // Variable to create a new list inside event
+            let li = document.createElement("li");
+            // Adds class "list-item" to the item
+            li.classList.add("list-item");
+            
+            // Makes sure the format is displayed as yyyy-mm-dd hh:mm
+            const timeFormat = ("sv-SE", {
+                hour: "2-digit",
+                minute: "2-digit",
+            });
+            // Applies aformentioned formating
+            const startDate = event.startDate.toLocaleDateString("sv-SE");
+            const startTime = event.startDate.toLocaleTimeString("sv-SE", timeFormat);
+            const endDate = event.endDate.toLocaleDateString("sv-SE");
+            const endTime = event.endDate.toLocaleTimeString("sv-SE", timeFormat);
+            // Creates inner html for event
+            li.innerHTML = `
+                <div class="event-details">
+                    <h3><strong>${event.title}</strong></h3>
+                        <p><strong>Start:</strong> ${startDate} ${startTime}</p>
+                    <p><strong>End:</strong> ${endDate} ${endTime}</p>
+                </div>`;
+            // Adds into event list ul
+            eventList.appendChild(li);
         });
+    } else {
+        let li = document.createElement("li");
+        // Adds class "list-item" to the item
+        li.classList.add("list-item");
 
-        const startDate = event.startDate.toLocaleDateString("sv-SE");
-        const startTime = event.startDate.toLocaleTimeString("sv-SE", timeFormat);
-        const endDate = event.endDate.toLocaleDateString("sv-SE");
-        const endTime = event.endDate.toLocaleTimeString("sv-SE", timeFormat);
         li.innerHTML = `
             <div class="event-details">
-                <h2><strong>Event</strong></h2>
-                <h3>${event.title}</h3>
-                    <p>Start: ${startDate} ${startTime}</p>
-                <p>End: ${endDate} ${endTime}</p>
-            </div>
-            `;
+                <h3><strong>No upcoming events</strong></h3>
+            </div>`;
         eventList.appendChild(li);
-        
-    });
+    }
+    
 }
-
-displayEvents()
